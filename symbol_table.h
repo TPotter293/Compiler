acf37d9743dfa78c04696f327c04297f703b94dc
JAguilar03 <97245033+JAguilar03@users.noreply.github.com>
2024-10-27 17:35:41 -0700

    Update symbol_table.h

--- symbol_table.h
+++ symbol_table.h
@@ -13,16 +13,16 @@ typedef struct {
 typedef struct {
     char *name;     // Identifier name
     char *type;     // Type (int, float, etc.)
+    int is_initialized; // New field to track initialization status
     FunctionInfo* functionInfo;
 } Symbol;
 
 // Declare symbol table functions
 void init_symbol_table();
 void insert_symbol(char *name, char *type, char** paramTypes, int paramCount, char* returnType);
-int lookup_symbol(char *name);
+Symbol* lookup_symbol(char *name);  // Ensure this matches the definition in symbol_table.c
 void print_symbol_table();
 void freeParamTypes(char** paramTypes, int count);
-
 void clean_up_symbol_table();
 
 #endif
